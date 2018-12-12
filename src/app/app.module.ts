5c5fca150b6d3b968d1a96f7841a0996f2220eb5
ChocolateAceCream <nuodi@hotmail.com>
2018-12-12 15:31:24 -0500

    working on singup page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -11,20 +14,22 @@ import { LoginComponent } from './auth/login/login.component';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SignupComponent,
-    LoginComponent,
-    HomeComponent
-  ],
-  imports: [
-      BrowserModule,
-	  BrowserAnimationsModule,
-	  BrowserAnimationsModule,
-      MaterialModule,
-      AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        SignupComponent,
+        LoginComponent,
+        HomeComponent,
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        BrowserAnimationsModule,
+        MaterialModule,
+        AppRoutingModule,
+        FlexLayoutModule,
+        FormsModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }

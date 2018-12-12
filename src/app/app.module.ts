1984cfd59991645c9c4c034c441f011d1f25fcdf
ChocolateAceCream <nuodi@hotmail.com>
2018-12-12 11:00:26 -0500

    added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { MaterialModule} from './material.module';
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
       BrowserModule,
 	  BrowserAnimationsModule,
 	  BrowserAnimationsModule,
-      MaterialModule
+      MaterialModule,
+      AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]

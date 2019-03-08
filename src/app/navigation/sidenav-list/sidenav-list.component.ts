4541c26c77a146171827721d8ccce3d27f2ab943
ChocolateAceCream <nuodi@hotmail.com>
2019-03-08 07:02:41 -0500

    edit sidenav filter layout

--- src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./sidenav-list.component.css']
 })
 export class SidenavListComponent implements OnInit {
+    sizes= [
+        {size: "11.5", checked: "false"},
+        {size: "11 ", checked: "false"},
+        {size: "10.5", checked: "false"},
+        {size: "10", checked: "false"},
+        {size: "9.5 ", checked: "false"},
+        {size: "9", checked: "false"},
+        {size: "8.5 ", checked: "false"},
+        {size: "8", checked: "false"},
+        {size: "7.5 ", checked: "false"}
+    ]
+
+    brands = [
+        {name: "adidas", checked: "false"},
+        {name: "airJordan", checked: "false"}
+    ]
 
     constructor() { }
 
     ngOnInit() {
+    }
+
+    onChange(data, $event) {
+        data.checked = $event.checked;
+        //console.log(this.brandAdidas, this.brandAirJordan, this.size11h);
     }
 
 }
--- src/app/product-list/most-popular/most-popular.component.ts
+++ src/app/product-list/most-popular/most-popular.component.ts
@@ -3,53 +3,51 @@ import { ViewChild,Component, OnInit, OnDestroy } from '@angular/core';
 import { ProductService } from '../product.service';
 import { Product } from '../product.model';
 import { Observable,Subscription } from 'rxjs';
-import { delay } from 'rxjs/operators';
 
 @Component({
-    selector: 'app-most-popular',
-    templateUrl: './most-popular.component.html',
-    styleUrls: ['./most-popular.component.css']
+	selector: 'app-most-popular',
+	templateUrl: './most-popular.component.html',
+	styleUrls: ['./most-popular.component.css']
 })
 export class MostPopularComponent implements OnInit, OnDestroy {
-    //products: Product[] = [];
-    length = 0;
-    pageIndex = 0;
-    pageSize = 10;
-    pageEvent: PageEvent;
+	products: Product[] = [];
+	length = 0;
+	pageIndex = 0;
+	pageSize = 10;
+	pageEvent: PageEvent;
 
-    obs: Observable<any>;
-    filterSubscription: Subscription;
-    dataSource = new MatTableDataSource<Product>();
+	obs: Observable<any>;
+	filterSubscription: Subscription;
+	dataSource = new MatTableDataSource<Product>();
 
-    @ViewChild(MatPaginator) paginator: MatPaginator;
+	@ViewChild(MatPaginator) paginator: MatPaginator;
 
-    constructor(private productService: ProductService) {
-        this.obs = this.dataSource.connect();
-        console.log(this.obs);
-        this.dataSource.data = this.productService.getMostPopularProducts();
-        this.dataSource.paginator = this.paginator;
-        this.filterSubscription = this.productService.filterString.subscribe(filter => {
-            this.dataSource.filter = filter.trim().toLowerCase();
-            //this.dataSource.filter = 'AJ1'.trim().toLowerCase();
-        });
-    }
+	constructor(private productService: ProductService) {
+		this.obs = this.dataSource.connect();
+		this.dataSource.data = this.productService.getMostPopularProducts();
+		this.dataSource.paginator = this.paginator;
+		this.filterSubscription = this.productService.filterString.subscribe(filter => {
+			this.dataSource.filter = filter.trim().toLowerCase();
+			//this.dataSource.filter = 'AJ1'.trim().toLowerCase();
+		});
 
-    ngOnInit() {
-        //delay(2);
-    }
+	}
 
-    addToCart(product: Product, size: number) {
-        let p = Object.assign({},product)
-        p.size=[size];
-        this.productService.addCartProduct(p);
-    }
+	ngOnInit() {
+	}
 
-    ngOnDestroy(): void {
-        if (this.dataSource) { this.dataSource.disconnect();}
-        this.filterSubscription.unsubscribe();
-    }
+	addToCart(product: Product, size: number) {
+		let p = Object.assign({},product)
+		p.size=[size];
+		this.productService.addCartProduct(p);
+	}
 
-    doFilter() {
-    }
+	ngOnDestroy(): void {
+		if (this.dataSource) { this.dataSource.disconnect();}
+		this.filterSubscription.unsubscribe();
+	}
+
+	doFilter() {
+	}
 
 }

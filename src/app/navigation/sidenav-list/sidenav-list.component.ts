c45494f5490e0ffdf15a3e94fb62df69c7a01de1
ChocolateAceCream <nuodi@hotmail.com>
2019-03-08 13:26:02 -0500

    add reset function on sidenave filter

--- src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -7,21 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidenavListComponent implements OnInit {
     sizes= [
-        {size: "11.5", checked: "false"},
-        {size: "11 ", checked: "false"},
-        {size: "10.5", checked: "false"},
-        {size: "10", checked: "false"},
-        {size: "9.5 ", checked: "false"},
-        {size: "9", checked: "false"},
-        {size: "8.5 ", checked: "false"},
-        {size: "8", checked: "false"},
-        {size: "7.5 ", checked: "false"}
-    ]
+        {size: "11.5", checked: false},
+        {size: "11 ", checked: false},
+        {size: "10.5", checked: false},
+        {size: "10", checked: false},
+        {size: "9.5 ", checked: false},
+        {size: "9", checked: false},
+        {size: "8.5 ", checked: false},
+        {size: "8", checked: false},
+        {size: "7.5 ", checked: false}
+    ];
 
     brands = [
-        {name: "adidas", checked: "false"},
-        {name: "airJordan", checked: "false"}
-    ]
+        {name: "adidas", checked: false},
+        {name: "airJordan", checked: false}
+    ];
 
     constructor() { }
 
@@ -30,7 +30,17 @@ export class SidenavListComponent implements OnInit {
 
     onChange(data, $event) {
         data.checked = $event.checked;
+        console.log(data);
         //console.log(this.brandAdidas, this.brandAirJordan, this.size11h);
+    }
+
+	onReset() {
+        this.sizes.forEach((element) => {
+            element.checked = false;
+        })
+        this.brands.forEach((element) => {
+            element.checked = false;
+        })
     }
 
 }
